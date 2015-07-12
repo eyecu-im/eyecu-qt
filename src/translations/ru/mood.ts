4363f34eaeda359e1efbad60e83c07da99895b74
Konstantin Kozlov <yagiza@yandex.ru>
2015-07-13 01:25:37 +0500

    Translations: ! Fixed Russian translation in Mood and Tune plugins.

--- src/translations/ru/mood.ts
+++ src/translations/ru/mood.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="ru_RU">
+<TS version="2.1" language="ru_RU">
 <context>
     <name>Mood</name>
     <message>
@@ -217,7 +217,7 @@
     </message>
     <message>
         <source>Interested</source>
-        <translation>Заинтересованне</translation>
+        <translation>Заинтересованное</translation>
     </message>
     <message>
         <source>Intoxicated</source>
