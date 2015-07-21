476efde8561ad588695672680de0ed1312a5464c
yagiza <yagiza@yandex.ru>
2015-07-21 20:13:51 +0500

    Translation scripts updated.

--- src/translations/tspush.cmd
+++ src/translations/tspush.cmd
@@ -2,3 +2,6 @@
 
 echo Pushing Russian translation to transifex
 tx.exe push -t -f --no-interactive -l ru
+
+echo Pushing source translation (en) to transifex
+tx.exe push -s -f --no-interactive -l en
