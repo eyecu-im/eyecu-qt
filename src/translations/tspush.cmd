140d7a89703fbc6a901bacae6a97982f7dce2ac3
Konstantin Kozlov <yagiza@yandex.ru>
2015-07-22 15:10:42 +0500

    Translations: + Configuration file updated: eyeCU plugins added. - Vacuum-IM-specific translation files removed from Deutsch and English locale directories. * TS push script updated.

--- src/translations/tspush.cmd
+++ src/translations/tspush.cmd
@@ -1,7 +1,18 @@
 @echo off
 
-echo Pushing Russian translation to transifex
-tx.exe push -t -f --no-interactive -l ru
-
 echo Pushing source translation (en) to transifex
 tx.exe push -s -f --no-interactive -l en
+
+if "%1" == "all" goto all
+
+echo Pushing Russian translation to transifex
+tx.exe push -t -f --no-interactive -l ru
+goto end
+
+:all
+echo Pushing all translations to transifex
+tx.exe push -t -f --no-interactive -l ru,pl,de,uk,es,nl,ja
+
+:end
+echo Done!
+
