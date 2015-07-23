f05fc78618868f5ab449eb9dbdd86081dd12498f
yagiza <yagiza@yandex.ru>
2015-07-23 22:18:51 +0500

    Autotranslate updated for English source translation.

--- src/tools/autotranslate/autotranslate.cpp
+++ src/tools/autotranslate/autotranslate.cpp
@@ -21,7 +21,7 @@ void myMessageHandler(QtMsgType type, const char *msg)
 			 break;
 		 case QtFatalMsg:
 			 fprintf(stderr, "Fatal: %s\n", msg);
-			 abort();
+			 exit(-1);
 	}
 }
 #else
@@ -57,20 +57,13 @@ int main(int argc, char *argv[])
 #endif
 	QCoreApplication app(argc, argv);
 
-	if (argc != 3)
+	if (argc != 2)
 	{
 		qCritical("Usage: autotranslate <destination-dir> <source-dir>.");
 		return -1;
 	}
 
-	QDir dstDir(app.arguments().value(1));
-	if (!dstDir.exists())
-	{
-		qCritical("Destination directory '%s' not found.",dstDir.dirName().toLocal8Bit().constData());
-		return -1;
-	}
-
-	QDir srcDir(app.arguments().value(2),"*.ts",QDir::Name,QDir::Files);
+	QDir srcDir(app.arguments().value(1),"*.ts",QDir::Name,QDir::Files);
 	if (!srcDir.exists())
 	{
 		qCritical("Source directory '%s' not found.",srcDir.dirName().toLocal8Bit().constData());
@@ -100,53 +93,37 @@ int main(int argc, char *argv[])
 						QDomElement sourceElem = messageElem.firstChildElement("source");
 						QDomElement translationElem = messageElem.firstChildElement("translation");
 						if (!sourceElem.isNull() && !translationElem.isNull())
-						{
-							QString sourceText = sourceElem.text();
-							if (messageElem.attribute("numerus") == "yes")
+							if (translationElem.attribute("type")=="unfinished" && translationElem.text().isEmpty())
 							{
-								QDomElement numerusEelem = translationElem.firstChildElement("numerusform");
-								while(!numerusEelem.isNull())
+								QString sourceText = sourceElem.text();
+								if (messageElem.attribute("numerus") == "yes")
+									qWarning("Untranslated numerus message: \"%s\"", sourceText.toLocal8Bit().constData());
+								else
 								{
-									numerusEelem.removeChild(numerusEelem.firstChild());
-									numerusEelem.appendChild(doc.createTextNode(sourceText));
-									numerusEelem = numerusEelem.nextSiblingElement("numerusform");
+									translationElem.removeChild(translationElem.firstChild());
+									translationElem.appendChild(doc.createTextNode(sourceText));
+									translationElem.removeAttribute("type");
 								}
 							}
-							else
-							{
-								translationElem.removeChild(translationElem.firstChild());
-								translationElem.appendChild(doc.createTextNode(sourceText));
-							}
-							translationElem.removeAttribute("type");
-						}
 						messageElem = messageElem.nextSiblingElement("message");
 					}
 					contextElem = contextElem.nextSiblingElement("context");
 				}
 				srcFile.close();
-
-				QFileInfo srcFileInfo(srcDir.absoluteFilePath(srcFile.fileName()));
-				QFile dstFile(dstDir.absoluteFilePath(srcFileInfo.fileName()));
-				if (dstFile.open(QFile::WriteOnly|QFile::Truncate))
+				if (srcFile.open(QFile::WriteOnly|QFile::Truncate))
 				{
-					dstFile.write(doc.toByteArray());
-					dstFile.close();
+					srcFile.write(doc.toByteArray());
+					srcFile.close();
 				}
 				else
-				{
-					qWarning("Failed to open destination file '%s' for write.",dstFile.fileName().toLocal8Bit().constData());
-				}
+					qWarning("Failed to open destination file '%s' for write.",srcFile.fileName().toLocal8Bit().constData());
 			}
 			else
-			{
 				qWarning("Invalid translation source file '%s'.",srcFile.fileName().toLocal8Bit().constData());
-			}
 			srcFile.close();
 		}
 		else
-		{
 			qWarning("Could not open translation source file '%s'.",srcFile.fileName().toLocal8Bit().constData());
-		}
 	}
 	return 0;
 }
