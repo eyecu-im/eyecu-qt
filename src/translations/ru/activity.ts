73d9132b10c736d71290f6ff58356789db29798a
Konstantin Kozlov <yagiza@yandex.ru>
2015-11-01 02:17:20 +0500

    Fix Russian translation for User Mood plugin
    Fix translation of "Having lunch" and "Having dinner" activity names

--- src/translations/ru/activity.ts
+++ src/translations/ru/activity.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="ru_RU" sourcelanguage="en">
+<TS version="2.1" language="ru_RU" sourcelanguage="en">
 <context>
     <name>Activity</name>
     <message>
@@ -133,11 +133,11 @@
     </message>
     <message>
         <source>Having dinner</source>
-        <translation>Обедаю</translation>
+        <translation>Ужинаю</translation>
     </message>
     <message>
         <source>Having lunch</source>
-        <translation>Ланч</translation>
+        <translation>Обедаю</translation>
     </message>
     <message>
         <source>Cycling</source>
