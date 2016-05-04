bc28dc2c138871f40dbfb2ec9a7a39c92b21abcc
Yagiza <yagiza@yandex.ru>
2016-05-04 19:28:34 +0600

    Update Qindows QtIFW package scripts.

--- src/packages/windows/QtIFW/buildpackage.cmd
+++ src/packages/windows/QtIFW/buildpackage.cmd
@@ -9,7 +9,7 @@ set packages=packages
 echo Creating base package
 rem call substver.cmd %packagename% %version%
 
-goto repo
+rem goto repo
 
 if exist "%qtdir%" goto exists
 echo No Qt installation found!
