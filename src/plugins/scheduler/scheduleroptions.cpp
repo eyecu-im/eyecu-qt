2f93cc333bd12198476acca4f4a45e3879315bb1
Yagiza <yagiza@yandex.ru>
2016-07-06 22:24:19 +0600

    Keep working on Scheduler plugin.

--- src/plugins/scheduler/scheduleroptions.cpp
+++ src/plugins/scheduler/scheduleroptions.cpp
@@ -35,27 +35,47 @@ void SchedulerOptions::onItemAdd()
 	{
 		qDebug() << "Ok";
 		SchedulerItem schedulerItem = itemDialog->getItem();
-		QTreeWidgetItem *item = new QTreeWidgetItem(ui->twSchedule);
+		QTreeWidgetItem *item = new QTreeWidgetItem();
 		item->setText(0, FAccountManager->findAccountByStream(schedulerItem.streamJid)->name());
-		item->setData(0, Qt::UserRole+1, schedulerItem.streamJid.full());
+		item->setData(0, Qt::UserRole, schedulerItem.streamJid.full());
 		item->setText(1, schedulerItem.contactJid.full());
-		item->setData(1, Qt::UserRole+1, schedulerItem.streamJid.full());
+		item->setData(1, Qt::UserRole, schedulerItem.streamJid.full());
 		item->setText(2, QString::number(schedulerItem.timeout));
 		item->setText(3, schedulerItem.message);
 		ui->twSchedule->addTopLevelItem(item);
 	}
+	itemDialog->deleteLater();
 }
 
 void SchedulerOptions::onItemEdit()
 {
-	SchedulerItemDialog *itemDialog = new SchedulerItemDialog(SchedulerItem(), FAccountManager, FRosterManager, this);
-	if (itemDialog->exec())
+	QTreeWidgetItem *item = ui->twSchedule->currentItem();
+	if (item)
 	{
-		qDebug() << "Ok";
+		SchedulerItem schedulerItem;
+		schedulerItem.streamJid	= item->data(0, Qt::UserRole).toString();
+		schedulerItem.contactJid = item->data(1, Qt::UserRole).toString();
+		schedulerItem.timeout	= item->text(2).toInt();
+		schedulerItem.message	= item->text(3);
+		SchedulerItemDialog *itemDialog = new SchedulerItemDialog(schedulerItem, FAccountManager, FRosterManager, this);
+		if (itemDialog->exec())
+		{
+			qDebug() << "Ok";
+			SchedulerItem schedulerItem = itemDialog->getItem();
+			item->setText(0, FAccountManager->findAccountByStream(schedulerItem.streamJid)->name());
+			item->setData(0, Qt::UserRole, schedulerItem.streamJid.full());
+			item->setText(1, schedulerItem.contactJid.full());
+			item->setData(1, Qt::UserRole, schedulerItem.streamJid.full());
+			item->setText(2, QString::number(schedulerItem.timeout));
+			item->setText(3, schedulerItem.message);
+		}
+		itemDialog->deleteLater();
 	}
 }
 
 void SchedulerOptions::onItemDelete()
 {
-
+	QTreeWidgetItem *item = ui->twSchedule->currentItem();
+	if (item)
+		delete ui->twSchedule->takeTopLevelItem(ui->twSchedule->indexOfTopLevelItem(item));
 }
