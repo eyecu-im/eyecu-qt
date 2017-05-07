b5deb232836b8e38a783a81825071f2425d9811c
Yagiza <yagiza@yandex.ru>
2017-05-07 09:39:18 +0600

    Fix minor typo in XHTML-IM plugin

--- src/translations/ru/geoloc.ts
+++ src/translations/ru/geoloc.ts
@@ -1,229 +1,230 @@
-<?xml version="1.0" encoding="utf-8"?>
+<?xml version='1.0' encoding='utf-8'?>
 <!DOCTYPE TS>
 <TS version="2.0" language="ru_RU">
-<context>
+  <context>
     <name>ContactProximityNotificationOptions</name>
     <message>
-        <source>Distance</source>
-        <translation>Расстояние</translation>
+      <source>Distance</source>
+      <translation>Расстояние</translation>
     </message>
     <message>
-        <source>Treshold</source>
-        <translation>Порог срабатывания</translation>
+      <source>Treshold</source>
+      <translation>Порог срабатывания</translation>
     </message>
     <message>
-        <source>Ignore own resources</source>
-        <translation>Игнорировать собственные ресурсы</translation>
+      <source>Ignore own resources</source>
+      <translation>Игнорировать собственные ресурсы</translation>
     </message>
     <message numerus="yes">
-        <source>meter(s)</source>
-        <comment>distance units</comment>
-        <translation>
-            <numerusform>метр</numerusform>
-            <numerusform>метра</numerusform>
-            <numerusform>метров</numerusform>
-        </translation>
+      <source>meter(s)</source>
+      <comment>distance units</comment>
+      <translation>
+        <numerusform>метр</numerusform>
+        <numerusform>метра</numerusform>
+        <numerusform>метров</numerusform>
+        <numerusform>метров</numerusform>
+      </translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>Geoloc</name>
     <message>
-        <source>User Location</source>
-        <translation>Местоположение Пользователя</translation>
+      <source>User Location</source>
+      <translation>Местоположение Пользователя</translation>
     </message>
     <message>
-        <source>Implements XEP-0080: User Location</source>
-        <translation>Реализует XEP-0080: Местоположение Пользователя</translation>
+      <source>Implements XEP-0080: User Location</source>
+      <translation>Реализует XEP-0080: Местоположение Пользователя</translation>
     </message>
     <message>
-        <source>S</source>
-        <translation>Ю</translation>
+      <source>S</source>
+      <translation>Ю</translation>
     </message>
     <message>
-        <source>N</source>
-        <translation>С</translation>
+      <source>N</source>
+      <translation>С</translation>
     </message>
     <message>
-        <source>W</source>
-        <translation>З</translation>
+      <source>W</source>
+      <translation>З</translation>
     </message>
     <message>
-        <source>E</source>
-        <translation>В</translation>
+      <source>E</source>
+      <translation>В</translation>
     </message>
     <message>
-        <source>Supports user Geolocation</source>
-        <translation>Поддерживает Местоположение Пользователя</translation>
+      <source>Supports user Geolocation</source>
+      <translation>Поддерживает Местоположение Пользователя</translation>
     </message>
     <message>
-        <source>User Location Notification</source>
-        <translation>Уведомление о Местоположении Пользователя</translation>
+      <source>User Location Notification</source>
+      <translation>Уведомление о Местоположении Пользователя</translation>
     </message>
     <message>
-        <source>Receives notification of current user Geolocation</source>
-        <translation>Получает уведомления о географическом местоположении пользователя</translation>
+      <source>Receives notification of current user Geolocation</source>
+      <translation>Получает уведомления о географическом местоположении пользователя</translation>
     </message>
     <message>
-        <source>Location</source>
-        <translation>Местоположение</translation>
+      <source>Location</source>
+      <translation>Местоположение</translation>
     </message>
     <message>
-        <source>Show contact on the map</source>
-        <translation>Показать контакт на карте</translation>
+      <source>Show contact on the map</source>
+      <translation>Показать контакт на карте</translation>
     </message>
     <message>
-        <source>F10</source>
-        <comment>Show contact on the map (chat)</comment>
-        <translation>F10</translation>
+      <source>F10</source>
+      <comment>Show contact on the map (chat)</comment>
+      <translation>F10</translation>
     </message>
     <message>
-        <source>m</source>
-        <comment>Short for &quot;meters&quot;</comment>
-        <translation>м</translation>
+      <source>m</source>
+      <comment>Short for "meters"</comment>
+      <translation>м</translation>
     </message>
     <message>
-        <source>Show user location icons</source>
-        <translation>Показывать иконки местоположения пользователя</translation>
+      <source>Show user location icons</source>
+      <translation>Показывать иконки местоположения пользователя</translation>
     </message>
     <message>
-        <source>Display user location icon</source>
-        <translation>Отображать иконку метоположения пользователя</translation>
+      <source>Display user location icon</source>
+      <translation>Отображать иконку метоположения пользователя</translation>
     </message>
     <message>
-        <source>Publish location</source>
-        <translation>Публиковать местоположение</translation>
+      <source>Publish location</source>
+      <translation>Публиковать местоположение</translation>
     </message>
     <message>
-        <source>Accuracy</source>
-        <translation>Точность</translation>
+      <source>Accuracy</source>
+      <translation>Точность</translation>
     </message>
     <message>
-        <source>Altitude</source>
-        <translation>Высота</translation>
+      <source>Altitude</source>
+      <translation>Высота</translation>
     </message>
     <message>
-        <source>Area</source>
-        <translation>Район</translation>
+      <source>Area</source>
+      <translation>Район</translation>
     </message>
     <message>
-        <source>Bearing</source>
-        <translation>Направление</translation>
+      <source>Bearing</source>
+      <translation>Направление</translation>
     </message>
     <message>
-        <source>Building</source>
-        <translation>Здание</translation>
+      <source>Building</source>
+      <translation>Здание</translation>
     </message>
     <message>
-        <source>Country</source>
-        <translation>Страна</translation>
+      <source>Country</source>
+      <translation>Страна</translation>
     </message>
     <message>
-        <source>Country code</source>
-        <translation>Код страны</translation>
+      <source>Country code</source>
+      <translation>Код страны</translation>
     </message>
     <message>
-        <source>Datum</source>
-        <translation>Датум</translation>
+      <source>Datum</source>
+      <translation>Датум</translation>
     </message>
     <message>
-        <source>Description</source>
-        <translation>Описание</translation>
+      <source>Description</source>
+      <translation>Описание</translation>
     </message>
     <message>
-        <source>Error</source>
-        <translation>Погрешность</translation>
+      <source>Error</source>
+      <translation>Погрешность</translation>
     </message>
     <message>
-        <source>Floor</source>
-        <translation>Этаж</translation>
+      <source>Floor</source>
+      <translation>Этаж</translation>
     </message>
     <message>
-        <source>Latitude</source>
-        <translation>Широта</translation>
+      <source>Latitude</source>
+      <translation>Широта</translation>
     </message>
     <message>
-        <source>Locality</source>
-        <translation>Населённый пункт</translation>
+      <source>Locality</source>
+      <translation>Населённый пункт</translation>
     </message>
     <message>
-        <source>Longitude</source>
-        <translation>Долгота</translation>
+      <source>Longitude</source>
+      <translation>Долгота</translation>
     </message>
     <message>
-        <source>Postal code</source>
-        <translation>Почтовый индекс</translation>
+      <source>Postal code</source>
+      <translation>Почтовый индекс</translation>
     </message>
     <message>
-        <source>Region</source>
-        <translation>Область/Край</translation>
+      <source>Region</source>
+      <translation>Область/Край</translation>
     </message>
     <message>
-        <source>Room</source>
-        <translation>Комната</translation>
+      <source>Room</source>
+      <translation>Комната</translation>
     </message>
     <message>
-        <source>Speed</source>
-        <translation>Скорость</translation>
+      <source>Speed</source>
+      <translation>Скорость</translation>
     </message>
     <message>
-        <source>Street</source>
-        <translation>Улица</translation>
+      <source>Street</source>
+      <translation>Улица</translation>
     </message>
     <message>
-        <source>Text</source>
-        <translation>Текст</translation>
+      <source>Text</source>
+      <translation>Текст</translation>
     </message>
     <message>
-        <source>Timestamp</source>
-        <translation>Временная метка</translation>
+      <source>Timestamp</source>
+      <translation>Временная метка</translation>
     </message>
     <message>
-        <source>TZO</source>
-        <translation>TZO</translation>
+      <source>TZO</source>
+      <translation>TZO</translation>
     </message>
     <message>
-        <source>URI</source>
-        <translation>URI</translation>
+      <source>URI</source>
+      <translation>URI</translation>
     </message>
     <message>
-        <source>%1: %2%3</source>
-        <translation>%1: %2%3</translation>
+      <source>%1: %2%3</source>
+      <translation>%1: %2%3</translation>
     </message>
     <message>
-        <source>&lt;b&gt;%1:&lt;/b&gt;&lt;span %4&gt;&amp;nbsp;%2%3&lt;/span&gt;</source>
-        <translation>&lt;b&gt;%1:&lt;/b&gt;&lt;span %4&gt;&amp;nbsp;%2%2&lt;/span&gt;</translation>
+      <source>&lt;b>%1:&lt;/b>&lt;span %4>&amp;nbsp;%2%3&lt;/span></source>
+      <translation>&lt;b>%1:&lt;/b>&lt;span %4>&amp;nbsp;%2%2&lt;/span></translation>
     </message>
     <message>
-        <source>%1: %2</source>
-        <translation>%1: %2</translation>
+      <source>%1: %2</source>
+      <translation>%1: %2</translation>
     </message>
     <message>
-        <source>&lt;b&gt;%1:&lt;/b&gt;&lt;span %3&gt;&amp;nbsp;%2&lt;/span&gt;</source>
-        <translation>&lt;b&gt;%1:&lt;/b&gt;&lt;span %3&gt;&amp;nbsp;%2&lt;/span&gt;</translation>
+      <source>&lt;b>%1:&lt;/b>&lt;span %3>&amp;nbsp;%2&lt;/span></source>
+      <translation>&lt;b>%1:&lt;/b>&lt;span %3>&amp;nbsp;%2&lt;/span></translation>
     </message>
     <message>
-        <source>&lt;b&gt;%1:&lt;/b&gt; %2</source>
-        <translation>&lt;b&gt;%1:&lt;/b&gt; %2</translation>
+      <source>&lt;b>%1:&lt;/b> %2</source>
+      <translation>&lt;b>%1:&lt;/b> %2</translation>
     </message>
     <message>
-        <source>&lt;b&gt;%1:&lt;/b&gt;&amp;nbsp;%2</source>
-        <translation>&lt;b&gt;%1:&lt;/b&gt;&amp;nbsp;%2</translation>
+      <source>&lt;b>%1:&lt;/b>&amp;nbsp;%2</source>
+      <translation>&lt;b>%1:&lt;/b>&amp;nbsp;%2</translation>
     </message>
     <message>
-        <source>&lt;b&gt;%1:&lt;/b&gt;&amp;nbsp;&lt;a href=&quot;%2&quot;&gt;%2&lt;/a&gt;</source>
-        <translation>&lt;b&gt;%1:&lt;/b&gt;&amp;nbsp;&lt;a href=&quot;%2&quot;&gt;%2&lt;/a&gt;</translation>
+      <source>&lt;b>%1:&lt;/b>&amp;nbsp;&lt;a href="%2">%2&lt;/a></source>
+      <translation>&lt;b>%1:&lt;/b>&amp;nbsp;&lt;a href="%2">%2&lt;/a></translation>
     </message>
     <message>
-        <source>When contact appears nearby</source>
-        <translation>Когда контакт оказывается поблизости</translation>
+      <source>When contact appears nearby</source>
+      <translation>Когда контакт оказывается поблизости</translation>
     </message>
     <message>
-        <source>Contact proximity notification</source>
-        <translation>Уведомление о близости контакта</translation>
+      <source>Contact proximity notification</source>
+      <translation>Уведомление о близости контакта</translation>
     </message>
     <message>
-        <source>Appeared nearby!</source>
-        <translation>Оказался поблизости!</translation>
+      <source>Appeared nearby!</source>
+      <translation>Оказался поблизости!</translation>
     </message>
-</context>
+  </context>
 </TS>
--- src/translations/ru/xhtmlim.ts
+++ src/translations/ru/xhtmlim.ts
@@ -1,6 +1,6 @@
 <?xml version='1.0' encoding='utf-8'?>
 <!DOCTYPE TS>
-<TS version="2.0" language="ru_RU">
+<TS version="2.1" language="ru_RU">
   <context>
     <name>AddLink</name>
     <message>
@@ -919,7 +919,7 @@
     </message>
     <message>
       <source>Default image format</source>
-      <translation>Фрмат изображения по умолчанию</translation>
+      <translation>Формат изображения по умолчанию</translation>
     </message>
     <message numerus="yes">
       <source>byte(s)</source>
